89274ad9e75f256575c865ee4f4e7b4706ccd17c
hazem200025 <153329457+hazem200025@users.noreply.github.com>
2024-01-27 13:30:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const cors = require('cors');
 const path = require("path");
 dotenv.config();
 mongoose.connect(
-  process.env.MONGO_URL,
+  "mongodb://hazgame96:7Yt5Oaf02tsXN8gx@ac-zwyjpdr-shard-00-00.1oznldw.mongodb.net:27017,ac-zwyjpdr-shard-00-01.1oznldw.mongodb.net:27017,ac-zwyjpdr-shard-00-02.1oznldw.mongodb.net:27017/?ssl=true&replicaSet=atlas-z62pow-shard-0&authSource=admin&retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("Connected to MongoDB");
