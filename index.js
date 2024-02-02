4c104d34c760cd61ba60f6c86b0441841b25d984
hazem200025 <153329457+hazem200025@users.noreply.github.com>
2024-02-02 01:24:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,12 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust the origin based on your needs
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(cors({ origin: '*' }));
 app.listen(process.env.PORT||8800, () => {
   console.log("Backend server is running!");
